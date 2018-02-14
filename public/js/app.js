a55b80c6b5fdabc7c6fcb658ace1c5048abd2559
JackelineGCP <gessenia_17@hotmail.com>
2018-02-13 23:57:44 -0500

    practice with fetch

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+// alert('Hola mundo desde mi server');
+const btn = document.querySelector("button");
+const priceSpan = document.querySelector("#price");
+let currency = 'EUR';
+const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+btn.addEventListener('click', function () {
+  fetch(url)
+    .then(function (response) {
+      console.log(response);
+      return response.json()
+    }).then(function (data) {
+      console.log(data.bpi[currency].rate);
+      priceSpan.innerText = data.bpi[currency].rate + currency;
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  /*   fetch(url)
+     .then(function(response){
+         console.log(response);
+         return response.json().then(function(data){
+             console.log(data);
+         })
+     })
+     .catch(function(error){
+         console.log(error);
+     }) */
+})
+/* fetch(url)
+    .then(function(response){
+        console.log(response);
+        console.log(response.json());
+    }) */
