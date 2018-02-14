b97c1e99c5118cd2cf649a1db78bb8f95cead09b
JackelineGCP <gessenia_17@hotmail.com>
2018-02-14 15:42:46 -0500

    agregando archivos

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,46 @@
-// alert('Hola mundo desde mi server');
-const btn = document.querySelector("button");
-const priceSpan = document.querySelector("#price");
-let currency = 'EUR';
-const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=c685e23f34b74c619a0895e03bf08727`;
 
-btn.addEventListener('click', function () {
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
   fetch(url)
     .then(function (response) {
-      console.log(response);
-      return response.json()
+      return response.json();
+      getJson();
     }).then(function (data) {
-      console.log(data.bpi[currency].rate);
-      priceSpan.innerText = data.bpi[currency].rate + currency;
+      console.log(data);
+      addNews(data);
     })
     .catch(function (error) {
-      console.log(error);
-    })
-  /*   fetch(url)
-     .then(function(response){
-         console.log(response);
-         return response.json().then(function(data){
-             console.log(data);
-         })
-     })
-     .catch(function(error){
-         console.log(error);
-     }) */
-})
-/* fetch(url)
-    .then(function(response){
-        console.log(response);
-        console.log(response.json());
-    }) */
+      handleError();
+    });
+});
+
+function getJson() {
+  const data = JSON.parse(this.responseText);
+  addNews(data);
+}
+
+function addNews(data) {
+  const articles = data.response.docs;
+  for (let index in articles) {
+    console.log(articles[index]);
+    var dataComplete = articles[index];
+    console.log(dataComplete);
+    let title = dataComplete.headline.main;
+    let snippet = dataComplete.snippet;
+    let li = document.createElement('li');
+    li.className = 'li';
+    li.innerHTML = snippet;
+    responseContainer.appendChild(li);
+  }
+};
+
+function handleError() {
+  console.log('Se ha presentado un error');
+}
